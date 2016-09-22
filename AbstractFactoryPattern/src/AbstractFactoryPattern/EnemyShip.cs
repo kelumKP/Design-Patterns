89a38df1cb8aaff720532791f4d8735defb7506a
kelumKP <ke.pedson@compsoc.lk>
2016-09-23 01:06:17 +0530

    makeShip() abstract method and toString() method added

--- AbstractFactoryPattern/src/AbstractFactoryPattern/EnemyShip.cs
+++ AbstractFactoryPattern/src/AbstractFactoryPattern/EnemyShip.cs
@@ -8,6 +8,8 @@ namespace AbstractFactoryPattern
     public abstract class EnemyShip
     {
         private string name;
+        public IESWeapon weapon;
+        public IESEngine engine;
         private double amtDamage;
 
         public string getName()
@@ -44,5 +46,16 @@ namespace AbstractFactoryPattern
         {
             Console.WriteLine(getName() + " attacks and does " + getDamage());
         }
+
+        public abstract void makeShip();
+
+        public string toString()
+        {
+            string infoOnShip = "The " + name + " has a top speed of " + engine + " and an attack power of " + weapon;
+
+            return infoOnShip;                       
+        }
+
+
     }
 }
