95400f049d34888dabf5b57a4b9dc521acf92e14
kelumKP <ke.pedson@compsoc.lk>
2016-09-23 09:58:54 +0530

    UFOEnemyShipBuilding class and its Content added

---
+++ AbstractFactoryPattern/src/AbstractFactoryPattern/UFOEnemyShipBuilding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    public class UFOEnemyShipBuilding : EnemyShipBuilding
+    {
+
+        protected override EnemyShip makeEnemyShip(string typeOfShip)
+        {
+            EnemyShip theEnemyShip = null;
+
+            if (typeOfShip.Equals("UFO"))
+            {
+                IEnemyShipFactory shipPartsFactory = new UFOEnemyShipFactory();
+                theEnemyShip = new UFOEnemyShip(shipPartsFactory);
+                theEnemyShip.setName("UFO Grunt Ship");
+            }
+
+            else if (typeOfShip.Equals("UFO BOSS"))
+            {
+                IEnemyShipFactory shipPartsFactory = new UFOBossEnemyShipFactory();
+                theEnemyShip = new UFOBossEnemyShip(shipPartsFactory);
+                theEnemyShip.setName("UFO Boss Ship");
+            }
+
+            return theEnemyShip;
+        }
+    }
+}
