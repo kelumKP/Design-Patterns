5eea5ff2d4f2fe66245355ff86def588958dd761
kelumKP <ke.pedson@compsoc.lk>
2016-09-23 09:57:30 +0530

    UFOBossEnemyShipFactory class and its Content added

---
+++ AbstractFactoryPattern/src/AbstractFactoryPattern/UFOBossEnemyShipFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace AbstractFactoryPattern
+{
+    public class UFOBossEnemyShipFactory : IEnemyShipFactory
+    {
+        public IESWeapon addESGun()
+        {
+            return new ESUFOGun();     // specific to regular UFO
+        }
+        public IESEngine addESEngine()
+        {
+            return new ESUFOEngine();
+        }
+    }
+}
