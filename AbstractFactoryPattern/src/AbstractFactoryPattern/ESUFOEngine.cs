327ffce00f1433a3f5cdbbcc89385b888c46a1e7
kelumKP <ke.pedson@compsoc.lk>
2016-09-23 09:47:54 +0530

    ESUFOEngine class and its Content added

---
+++ AbstractFactoryPattern/src/AbstractFactoryPattern/ESUFOEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    public class ESUFOEngine : IESEngine
+    {
+        public string toString()
+        {
+            return " 100 mph";
+
+        }
+    }
+}
