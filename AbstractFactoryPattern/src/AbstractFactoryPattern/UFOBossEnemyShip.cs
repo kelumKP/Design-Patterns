8dc701d2944443ac3e69f8e69d016df9658aef89
kelumKP <ke.pedson@compsoc.lk>
2016-09-23 09:56:31 +0530

    UFOBossEnemyShip class and its Content added

---
+++ AbstractFactoryPattern/src/AbstractFactoryPattern/UFOBossEnemyShip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    internal class UFOBossEnemyShip : EnemyShip
+    {
+        IEnemyShipFactory shipFactory;
+
+        public UFOBossEnemyShip(IEnemyShipFactory shipFactory)
+        {
+            this.shipFactory = shipFactory;
+        }
+
+        public override void makeShip()
+        {
+            Console.WriteLine("Making boss enemy ship " + getName());
+            weapon = shipFactory.addESGun();
+            engine = shipFactory.addESEngine();
+        }
+    }
+}
