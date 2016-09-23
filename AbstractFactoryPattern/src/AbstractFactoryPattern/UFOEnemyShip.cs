58e346ed34694a7fde7df827ba08d203fadef546
kelumKP <ke.pedson@compsoc.lk>
2016-09-23 09:58:14 +0530

    UFOEnemyShip class and its Content added

---
+++ AbstractFactoryPattern/src/AbstractFactoryPattern/UFOEnemyShip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public class UFOEnemyShip : EnemyShip
+    {
+
+        IEnemyShipFactory shipFactory;
+
+        public UFOEnemyShip(IEnemyShipFactory shipFactory)
+        {
+            this.shipFactory = shipFactory;
+        }
+
+        public override void makeShip()
+        {
+            Console.WriteLine("Making enemy ship " + getName());
+            weapon = shipFactory.addESGun();
+            engine = shipFactory.addESEngine();
+        }
+
+    }
+}
